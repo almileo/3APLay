147e51ad5fbc53136517e1c09c7429006417a751
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-07-28 11:36:40 -0300

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
 
+var search = document.getElementById("search"),
+    food = document.getElementsByTagName("span"),
+    forEach = Array.prototype.forEach;
 
+search.addEventListener("keyup", function(e){
+    var choice = this.value;
+  
+    forEach.call(food, function(f){
+        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1){
+            f.parentNode.style.display = "none";  
+        } else{
+            f.parentNode.style.display = "block"; 
+        }
+                  
+    });
+}, false);
