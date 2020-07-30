f2faab850c4f1be18bbe5ceb08a340789ca4275b
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-07-30 12:48:13 -0300

    se agrega primer carousel de accion automaticamente

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
 
+leerPeliculas();
 
+function leerPeliculas(){
+    if(localStorage.length > 0){
+        let listaPeliculasSeries = JSON.parse(localStorage.getItem("keyPelicula"));
+
+        
+        let imagenesPeliculas = document.getElementById("imagenesAccion");
+
+        let codHTML = "";
+        
+        for (let i in listaPeliculasSeries){
+            codHTML = ` <div class="col-sm-3 my-2">
+                            <a href="error404.html"><img src="img/categorias/accion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Spider-man de regreso a casa"></a>
+                        </div>`;
+            
+                        imagenesPeliculas.innerHTML += codHTML;
+        }
+    }
+}
