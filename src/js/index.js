7f3b2bb67acd785deb2a6e2a41f5d0e14aed2791
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-07-31 00:15:44 -0300

    se deja funcionando el primer carousel Accion, falta replicar codigo para las otras categorias y hacer el codigo para que distribuya solo en las categorias

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-import '../css/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "../css/style.css";
 
 leerPeliculas();
 
-function leerPeliculas(){
-    if(localStorage.length > 0){
-        let listaPeliculasSeries = JSON.parse(localStorage.getItem("keyPelicula"));
+function leerPeliculas() {
+  if (localStorage.length > 0) {
+    let listaPeliculasSeries = JSON.parse(localStorage.getItem("keyPelicula"));
 
-        
-        let imagenesPeliculas = document.getElementById("imagenesAccion");
+    let imagenesPeliculas = document.getElementById("imagenesAccion");
+    let imagenesPeliculasDos = document.getElementById("imagenesAccionDos");
 
-        let codHTML = "";
-        
-        for (let i in listaPeliculasSeries){
+    let codHTML = "";
+
+    for (let i in listaPeliculasSeries) {
+        if (i < 4){
             codHTML = ` <div class="col-sm-3 my-2">
                             <a href="error404.html"><img src="img/categorias/accion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Spider-man de regreso a casa"></a>
                         </div>`;
-            
-                        imagenesPeliculas.innerHTML += codHTML;
+
+        imagenesPeliculas.innerHTML += codHTML;  
+        }   
+        
+        if (i > 3 && i < 8){
+            codHTML = ` <div class="col-sm-3 my-2">
+                            <a href="error404.html"><img src="img/categorias/accion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Spider-man de regreso a casa"></a>
+                        </div>`;
+
+        imagenesPeliculasDos.innerHTML += codHTML; 
         }
-    }
+    } 
+  }
 }
+
+
+
+
