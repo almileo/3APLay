a360d7fab30e7c0bc742b31852cd6e4fda60cd8c
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-07-31 13:28:46 -0300

    se deja funcion switch funncionando, solo hay que tener en cuenta los codigos al cargar las peliculas

--- src/js/index.js
+++ src/js/index.js
@@ -18,78 +18,83 @@ function leerPeliculas() {
     let terrorDos = document.getElementById("imagenTerrorDos");
 
     let codHTML = "";
-    //Imagenes de Accion
-    for (let i in listaPeliculasSeries) {
-    }
-    // Imagenes de Comedia
-    for (let i in listaPeliculasSeries) {
-    }
-    //Imagenes Ciencia Ficcion
+    
+    
+    //Imagenes 
     for (let i in listaPeliculasSeries) {
       switch (listaPeliculasSeries[i].categoria) {
         case "Accion":
-          codHTML = ` <div class="col-sm-3 my-2">
+          if(listaPeliculasSeries[i].codigo == 1 || listaPeliculasSeries[i].codigo == 2 || listaPeliculasSeries[i].codigo == 3 || listaPeliculasSeries[i].codigo == 4){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/accion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Spider-man de regreso a casa"></a>
                                 </div>`;
 
           imagenesPeliculas.innerHTML += codHTML;
+          }
 
-          codHTML = ` <div class="col-sm-3 my-2">
+          if(listaPeliculasSeries[i].codigo == 5 || listaPeliculasSeries[i].codigo == 6 || listaPeliculasSeries[i].codigo == 7 || listaPeliculasSeries[i].codigo == 8){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/accion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Spider-man de regreso a casa"></a>
                                 </div>`;
 
           imagenesPeliculasDos.innerHTML += codHTML;
-
+          }
           break;
 
         case "Comedia":
-          codHTML = ` <div class="col-sm-3 my-2">
+          if (listaPeliculasSeries[i].codigo == 9 || listaPeliculasSeries[i].codigo == 10 || listaPeliculasSeries[i].codigo == 11 || listaPeliculasSeries[i].codigo == 12){
+            codHTML = ` <div class="col-sm-3 my-2">
                                      <a href="error404.html"><img src="img/categorias/comedia/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Ahora son trece"></a>
                                 </div>`;
 
           comedia.innerHTML += codHTML;
+          }
 
-          codHTML = ` <div class="col-sm-3 my-2">
+          if(listaPeliculasSeries[i].codigo == 13 || listaPeliculasSeries[i].codigo == 14 || listaPeliculasSeries[i].codigo == 15 || listaPeliculasSeries[i].codigo == 16){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/comedia/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Ahora son trece"></a>
                                 </div>`;
 
           comediaDos.innerHTML += codHTML;
-
+          } 
           break;
 
         case "Ciencia Ficcion":
-          codHTML = ` <div class="col-sm-3 my-2">
+          if (listaPeliculasSeries[i].codigo == 17 || listaPeliculasSeries[i].codigo == 18 || listaPeliculasSeries[i].codigo == 19 || listaPeliculasSeries[i].codigo == 20){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/ficcion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Chappie"></a>
                                 </div>`;
 
           cienciaFiccion.innerHTML += codHTML;
+          }
 
-          codHTML = ` <div class="col-sm-3 my-2">
+          if (listaPeliculasSeries[i].codigo == 21 || listaPeliculasSeries[i].codigo == 22 || listaPeliculasSeries[i].codigo == 23 || listaPeliculasSeries[i].codigo == 24){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/ficcion/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Chappie"></a>
                                 </div>`;
 
           cienciaFiccionDos.innerHTML += codHTML;
-
+          }
           break;
 
         case "Terror":
-          codHTML = ` <div class="col-sm-3 my-2">
-                                    <a href="error404.html"><img src="img/categorias/terror/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Dulce Venganza"></a>
-                                </div>`;
+          if (listaPeliculasSeries[i].codigo == 25 || listaPeliculasSeries[i].codigo == 26 || listaPeliculasSeries[i].codigo == 27 || listaPeliculasSeries[i].codigo == 28){
+            codHTML = ` <div class="col-sm-3 my-2">
+            <a href="error404.html"><img src="img/categorias/terror/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Dulce Venganza"></a>
+        </div>`;
 
-          terror.innerHTML += codHTML;
+            terror.innerHTML += codHTML;
+          }
 
-          codHTML = ` <div class="col-sm-3 my-2">
+          if (listaPeliculasSeries[i].codigo == 29 || listaPeliculasSeries[i].codigo == 30 || listaPeliculasSeries[i].codigo == 31 || listaPeliculasSeries[i].codigo == 32){
+            codHTML = ` <div class="col-sm-3 my-2">
                                     <a href="error404.html"><img src="img/categorias/terror/${listaPeliculasSeries[i].imagen}" class="d-block w-100 rounded efectosImg" alt="Dulce Venganza"></a>
                                 </div>`;
 
           terrorDos.innerHTML += codHTML;
-
+          }   
           break;
       }
-    }
-    // Imagenes Terror
-    for (let i in listaPeliculasSeries) {
     }
   }
 }
