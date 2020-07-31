9deeda3fe2de78bdfbf11d0044d5be0575bb39d4
José A. Burgos <jose_7892@hotmail.com>
2020-07-31 11:10:05 -0300

    Se resuelven conflictos

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
 
+console.log("dsd index.js")
+console.log(codigo.value);
 
+let categoriaAccion = document.getElementById('categoriaAccion');
+console.log(categoriaAccion);
