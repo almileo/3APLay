b9ce292333026cc6466c0aeafd08e0de3c22430e
José A. Burgos <jose_7892@hotmail.com>
2020-08-02 13:39:59 -0300

    Se enlaza modal de Recupero Contraseña. Se agrega validación general, y validaciones html

--- src/js/login.js
+++ src/js/login.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
 import Swal from 'sweetalert2';
+import $ from 'jquery';
 
 let user = document.getElementById('nombreLogin');
 let clave = document.getElementById('passwLogin');
@@ -16,43 +17,80 @@ function usuario() {
     let expresion = /[a-z]/;
     if (user.value != "" && isNaN(user.value) && expresion.test(user.value)) {
         user.className = "form-control is-valid";
+        return true;
     } else {
         user.className = "form-control is-invalid";
+        return false;
     }
 }
 
 //validar contraseña
 function password() {
-    let caracteres = 8;
-    if (clave.value.length >= caracteres) {
+    const longitud = 8;
+    if (clave.value.length >= longitud) {
         clave.className = "form-control is-valid";
+        return true;
     } else {
         clave.className = "form-control is-invalid";
+        return false;
     }
 }
 
-//validar email de modal recuperar contraseña
+// VALIDAR EMAIL DE MODAL RECUPERAR CONTRASEÑA
 function recEmail() {
     let emailExpre = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-    if (correo.value != emailExpre.test(correo.value)) {
+    if (correo.value != "" && emailExpre.test(correo.value)) {
         correo.className = "form-control is-valid";
+        return true;
     } else {
         correo.className = "form-control is-invalid";
+        return false;
     }
 }
 
-//ingresar como administrador e ir a admin.html
+// VALIDACION GENERAL
 window.admin = function (event) {
-    event.preventDefault()
+    event.preventDefault();
+    if (usuario() &&
+        password()) {
+        console.log("OK");
+        validarCuentaAdmin(event);
+    } else {
+        console.log("ERROR!");
+    }
+}
+
+// VALIDAR EMAIL DE RECUPERO PASS
+window.recuperoEmail = function (event) {
+    event.preventDefault();
+    if (recEmail()) {
+        console.log("email OK")
+        let modalRecuperoContrasenia = document.getElementById('modalRecuperoContrasenia');
+        Swal.fire(
+            'Perfecto!',
+            'Tus datos se enviaron correctamente!',
+            'success'
+            );
+            $(modalRecuperoContrasenia).modal('hide');
+        } else {
+        console.log("email NO OK")
+    }
+}
+
+// VALIDAR ADMIN E IR A admin.html
+function validarCuentaAdmin(event) {
+    event.preventDefault();
     let userAdm = "admin-play";
     let passAdm = "rolling2020";
     if (user.value == userAdm && clave.value == passAdm) {
         window.location.replace("./admin.html");
     } else {
+        user.className = "form-control";
+        clave.className = "form-control";
         Swal.fire({
             icon: 'error',
             title: 'Tus datos son incorrectos.',
             text: 'Por favor, procure escribir los datos requeridos de manera correcta.',
-        })
-    }
+        });
+    };
 }
